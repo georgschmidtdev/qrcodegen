8fa4d7acaac42122acee15533ff708c3a329f4a2
georgschmidtdev <56390643+georgschmidtdev@users.noreply.github.com>
2020-02-13 02:03:20 +0100

    format dependent size selection implemented

--- generateQrCode.js
+++ generateQrCode.js
@@ -1,10 +1,45 @@
+let submitForm;
+
+let selectFormat;
+
+let selectSizeImg;
+
+let selectSizeVector;
+
+let selectColor;
+
+let selectBgColor;
+
 document.addEventListener("DOMContentLoaded", assignVariables, false);
 
 function assignVariables(){
 
-    let submitForm = document.getElementById('submitForm');
+    submitForm = document.getElementById('submitForm');
 
     submitForm.addEventListener('submit', handleSubmit);
+
+
+    selectFormat = document.getElementById('selectFormat');
+
+    selectFormat.addEventListener('change', changeSizeOptions);
+
+    selectSizeImg = document.getElementById('selectSizeImg');
+
+    selectSizeVector = document.getElementById('selectSizeVector');
+
+    selectSizeImg.style.display = "none";
+
+    selectSizeVector.style.display = "none";
+
+
+    selectColor = document.getElementById('selectColor');
+
+    selectColor.addEventListener('change', changeColVis);
+
+
+    selectBgColor = document.getElementById('selectBgColor');
+
+    selectBgColor.addEventListener('change', changeBgColVis);
 }
 
 function handleSubmit(event){
@@ -18,20 +53,34 @@ function handleSubmit(event){
     createParameters(submitQuery);
 }
 
+function changeSizeOptions(event){
+    
+    event.preventDefault();
+
+    if(selectFormat.value == "svg" || selectFormat.value == "eps"){
+
+        selectSizeImg.style.display = "none";
+
+        selectSizeVector.style.display = "block";
+    }else
+    if(selectFormat.value == "png" || selectFormat.value == "gif" || selectFormat == "jpg"){
+
+        selectSizeVector.style.display = "none";
+
+        selectSizeImg.style.display = "block";
+    }
+}
+
 function createParameters(UrlToEncode){
 
     let url = encodeURI(UrlToEncode);
 
-    let selectFormat = document.getElementById('selectFormat');
-
     let selectSize = document.getElementById('selectSize');
 
-    let selectColor = document.getElementById('selectColor');
     let customColR = document.getElementById('customColR');
     let customColG = document.getElementById('customColG');
     let customColB = document.getElementById('customColB');
 
-    let selectBgColor = document.getElementById('selectBgColor');
     let customBgColR = document.getElementById('customBgColR');
     let customBgColG = document.getElementById('customBgColG');
     let customBgColB = document.getElementById('customBgColB');
@@ -69,10 +118,10 @@ function extractDomain(url, endpoint){
 
     var domain = url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
 
-    displayCode(url, domain, endpoint);
+    displayCode(url, domain, endpoint, scrollToResult);
 }
 
-function displayCode(url, domain, endpoint){
+function displayCode(url, domain, endpoint, callback){
 
     let qrWrapper = document.getElementById('qrWrapper');
 
@@ -84,4 +133,13 @@ function displayCode(url, domain, endpoint){
         <img src="${endpoint}" alt="${url} QR-Code" id="qrCode">
     </a>
     `;
+
+    callback();
+}
+
+function scrollToResult(){
+
+    let wrapper = document.getElementById('qrWrapper');
+
+    wrapper.scrollIntoView({block: "end", behavior: 'smooth'});
 }
