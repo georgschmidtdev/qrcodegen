310b9fbc4b1ead79339a78bf86a2e678f50e26d9
georgschmidtdev <56390643+georgschmidtdev@users.noreply.github.com>
2020-02-13 04:40:40 +0100

    Service worker implemented

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function(e) {
+
+    e.waitUntil(
+
+        caches.open('airhorner').then(function(cache) {
+
+            return cache.addAll([
+
+                '/',
+                '/index.html',
+                '/index.html?homescreen=1',
+                '/?homescreen=1',
+                '/styles.css',
+                '/generateQrCode.js',
+                '/img/qrify_100.png',
+                '/img/qrify_200.png',
+                '/img/qrify_300.png',
+                '/img/qrify_400.png',
+                '/img/qrify_500.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+
+    console.log(event.request.url);
+
+    event.respondWith(
+
+        caches.match(event.request).then(function(response) {
+
+            return response || fetch(event.request);
+        })
+    );
+});
